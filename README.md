d58b8b263434972ae52cb635f3ed4cf8aedcee74
Simon Soriano <simonsp@uber.com>
2020-04-10 15:24:39 -0500

    Initial notebook commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trading - Technical analysis
+
+## Setting up the env
+- `pip install -r requirements.txt`
+
+## Running the notebook
+
+`jupyter notebook`
+
--- main.py
+++ main.py
@@ -2,25 +2,19 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-
 companies = ['AAPL','FB','GOOG','F','TSLA']
 listofdf = []
 for item in companies:
     histprices = requests.get(f"https://financialmodelingprep.com/api/v3/historical-price-full/{item}?serietype=line")
     histprices = histprices.json()
 
-#Parse the API response and select only last 600 days of prices
+    #Parse the API response and select only last 600 days of prices
     histprices = histprices['historical'][-600:]
-
-#Convert from dict to pandas datafram
-
+    #Convert from dict to pandas datafram
     histpricesdf = pd.DataFrame.from_dict(histprices)
-
-#rename column
+    #rename column
     histpricesdf = histpricesdf.rename({'close': item}, axis=1)
-
-#append all dfs to list
+    #append all dfs to list
     listofdf.append(histpricesdf)
 
 #set index of each DataFrame by common column before concatinatinghtem
@@ -32,7 +26,6 @@ histpriceconcat = pd.concat(dfs,axis=1)
 histpriceconcat = histpriceconcat/histpriceconcat.iloc[0]
 
 
-
 for i, col in enumerate(histpriceconcat.columns):
     histpriceconcat[col].plot()
 
@@ -40,4 +33,5 @@ plt.title('Price Evolution Comparison')
 
 plt.xticks(rotation=70)
 plt.legend(histpriceconcat.columns)
-plt.savefig('foo1.png', bbox_inches='tight')
+plt.gcf()
+#plt.savefig('foo1.png', bbox_inches='tight')
